7ec754dd6d05234e45a581dc3fe75c0144e166b7
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-13 18:05:12 -0300

    create db model tables

---
+++ prisma/migrations/20220913210434_create_tests_categories_and_teachers_disciplines_relation/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `categoryId` to the `tests` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `teacherDisciplineId` to the `tests` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "tests" ADD COLUMN     "categoryId" INTEGER NOT NULL,
+ADD COLUMN     "teacherDisciplineId" INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tests" ADD CONSTRAINT "tests_teacherDisciplineId_fkey" FOREIGN KEY ("teacherDisciplineId") REFERENCES "teachersDisciplines"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
