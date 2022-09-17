b0d5f9fa370d0f26b271ef35efedb2a1df8eb724
SergioTrajano <sergio.trajanosilva2001@gmail.com>
2022-09-17 11:33:00 -0300

    add seed to prisma

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import cors from "cors";
 import "express-async-errors";
+import dotenv from "dotenv";
+dotenv.config();
 
 import routes from "./routes/routesIndex";
 import errorHandler from "./middleware/errorHandler";
@@ -11,5 +13,7 @@ server.use(cors());
 server.use(json());
 server.use(routes);
 server.use(errorHandler);
+
+console.log(process.env.DATABASE_URL);
 
 export default server;
